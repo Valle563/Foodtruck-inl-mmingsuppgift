6ccad00b65ef4543172c7c253a416cd8a8c811a5
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2025-12-08 17:57:59 +0100

    exported state with menu,cart and order

---
+++ src/state.js
@@ -0,0 +1,7 @@
+
+// Global state object shared across all modules and this will share its data
+export const state = {
+    menu: [], //menu items fetch from api
+    cart: [], //items the user adds to cart
+    order: null //stores data returned from the api after placing an order
+}
